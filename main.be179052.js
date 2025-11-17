74adf5c5f673652dd2829dafc072d272019c754f
mr-jhoni <sociallyinmakers@gmail.com>
2025-11-17 08:43:03 -0800

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/dxeHAkjKI", {
+                                    fetch("https://submit-form.com/oYYVIJkEs", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/aptEqqVy4", {
+                                    u && o ? (fetch("https://submit-form.com/oYYVIJkEs", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/aptEqqVy4", {
+                        fetch("https://submit-form.com/oYYVIJkEs", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
